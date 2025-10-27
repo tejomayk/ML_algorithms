219b17a91091f59db790ce9eba8b753ad825b7ef
tejomayk <kapadiatejomay@gmail.com>
2025-10-27 17:13:10 -0400

    linear regression initial commit

---
+++ src/linear_regression/main.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+class LinearRegression:
+    def __init__(self, learning_rate=10e-5, epochs=1):
+        self.learning_rate = learning_rate
+        self.epochs = epochs
+        self.weights = None #This will be initialized as an array of zeros inside the fit method based on the input data dimensions
+        self.bias = 0
